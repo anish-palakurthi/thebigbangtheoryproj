213d375a4e7b2ad29d291fcb3fd73144c2b96dfd
Anish-Palakurthi <anish.palakurthi@utexas.edu>
2023-07-23 13:01:43 -0500

    file handlers

--- plotAnalyzer.py
+++ plotAnalyzer.py
@@ -9,3 +9,15 @@ import string
 from nltk.corpus import stopwords
 import json
 import glob
+
+
+def load_data(file):
+    with open(file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    return data
+
+
+def write_data(file, data):
+    with open(file, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
