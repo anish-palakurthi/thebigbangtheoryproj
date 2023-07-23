528e06e71f1025f417c7eb1df746dd00d7ed2b27
Anish-Palakurthi <anish.palakurthi@utexas.edu>
2023-07-23 13:00:03 -0500

    added dependencies

---
+++ plotAnalyzer.py
@@ -0,0 +1,11 @@
+# main runner file for topic modeling via tf-idf w/ scikit-learn in python
+
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.metrics import adjusted_rand_score
+import string
+from nltk.corpus import stopwords
+import json
+import glob
