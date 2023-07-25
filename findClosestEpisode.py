331f2d0421052983bc319225680ccc421086bf1d
Anish-Palakurthi <anish.palakurthi@utexas.edu>
2023-07-24 19:59:27 -0500

    added docs

--- findClosestEpisode.py
+++
@@ -1,2 +0,0 @@
-import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
--- tfidfAnalyzer.py
+++ tfidfAnalyzer.py
@@ -1,14 +1,9 @@
 # main runner file for topic modeling via tf-idf w/ scikit-learn in python
-
 # dependencies
-import re
-import glob
 import json
-import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
-from sklearn.metrics import adjusted_rand_score
 from sklearn.decomposition import PCA
 import string
 from nltk.corpus import stopwords
@@ -50,12 +45,13 @@ def remove_stops(text, stops):
 
     return cleaned
 
-
 # customize stop words to be removed
+
+
 def clean_text(plots):
     stops = stopwords.words("english")
     stops += ["raj", "leonard", "howard", "penny", "bernadette", "sheldon", "stuart", "amy",
-              "koothrappali", "wolowitz", "hofstadter", "cooper", "fowler", "rostenkowski", "bloom"]
+              "koothrappali", "wolowitz", "hofstadter", "cooper", "fowler", "rostenkowski", "bloom", "title", "reference"]
     final = []
 
     # cleans each episode's plot and appends to final list
@@ -66,16 +62,28 @@ def clean_text(plots):
     return final
 
 
+# loads descriptions of episodes
 episodes = load_data("plots.json")
 
-names = [str(episodes["Season"]) + ";" + str(episodes["No. inseason"])
-         for episodes in episodes]
-plots = [episodes["plot"] for episodes in episodes]
+# episode titles in the form of "Season;Episode"
+names = [str(episode["Season"]) + ";" + str(episode["No. inseason"])
+         for episode in episodes]
 
+# maps episode titles to their index in the names array
+names_map = {}
+for index in range(len(names)):
+    names_map[names[index]] = index
+
+# episode summaries
+plots = [episode["plot"] for episode in episodes]
+
+# episode summaries in the form of "Season;Episode\nSummary"
+summaries = [str(episode["Season"]) + ";" + str(episode["No. inseason"]
+                                                ) + "\n" + str(episode["plot"]) for episode in episodes]
+
+# loads episode scripts
 episodes_data = {}
-
 data = load_data("scripts.json")
-
 
 for line in data:
     episode_title = line["episode_name"]
@@ -93,17 +101,16 @@ for lines in episodes_data.values():
     episode_string = "".join(lines)
     episodes_array.append(episode_string)
 
-
+# clean up the episode scripts and combine them with the episode summaries
 for i in range(len(episodes_array)):
     episodes_array[i] = episodes_array[i].replace("\\n", " ")
     episodes_array[i] = episodes_array[i].replace("\\", "")
     episodes_array[i] = episodes_array[i].replace("  ", " ")
     plots[i] = plots[i] + " " + episodes_array[i]
 
-
 cleaned_plots = clean_text(plots)
 
-
+# TF-IDF Vectorizer parameters
 vectorizer = TfidfVectorizer(lowercase=True,
                              max_features=100,  # max number of words to keep
                              max_df=0.8,  # ignore words that appear in more than 80% of the documents
@@ -113,10 +120,8 @@ vectorizer = TfidfVectorizer(lowercase=True,
                              stop_words='english'  # remove stop words again just in case
                              )
 
-
+# training the vectorizer
 vectors = vectorizer.fit_transform(cleaned_plots)
-
-
 feature_names = vectorizer.get_feature_names_out()
 
 dense = vectors.todense()
@@ -137,9 +142,7 @@ for desc in dense_list:
 
     allKeywords.append(keywords)
 
-
 # K-Means Clustering ### -- limits each desc to one cluster/topic -- LDA will improve this
-
 cluster_num = 10
 
 # clusters descriptions into 20 clusters
@@ -147,16 +150,14 @@ model = KMeans(n_clusters=cluster_num,
                init='k-means++', max_iter=100, n_init=1)
 model.fit(vectors)
 
-
 order_centroids = model.cluster_centers_.argsort()[:, ::-1]
 terms = vectorizer.get_feature_names_out()
 
 ### PCA ###
-
+# preps for 2d visualization of clusters
 kmean_indices = model.fit_predict(vectors)
 pca = PCA(n_components=2)
 scatter_plot_points = pca.fit_transform(vectors.toarray())
-
 
 colors = ["r", "b", "g", "c", "m", "y", "k", "w", "orange", "purple", "pink",
           "brown", "gray", "olive", "cyan", "lime", "teal", "navy", "maroon", "gold"]
@@ -164,23 +165,28 @@ colors = ["r", "b", "g", "c", "m", "y", "k", "w", "orange", "purple", "pink",
 x_axis = [o[0] for o in scatter_plot_points]
 y_axis = [o[1] for o in scatter_plot_points]
 
-
 fig, ax = plt.subplots(figsize=(50, 50))
 ax.scatter(x_axis, y_axis, c=[colors[d] for d in kmean_indices])
-
 
 for i, txt in enumerate(names):
     ax.annotate(txt[0:5], (x_axis[i], y_axis[i]))
 
-
 plt.savefig("clusters.png")
 
+# takes user input for episode and number of recommendations
+user_episode = input("Enter the name of an episode: ")
+user_ep_index = names_map[user_episode]
+num_recs = int(input("Enter how many recommendations you want: "))
+num_recs_adj = num_recs * -1 - 1
 
-target_vector = vectors[0]
+target_vector = vectors[user_ep_index]
 sims = cosine_similarity(target_vector, vectors)
-closestFive = np.argsort(sims)[0][-6:-1]
+closest = np.argsort(sims)[0][num_recs:-1]
 
-for i in range(5):
-    print(names[closestFive[i]])
-    print(plots[closestFive[i]])
+print("Requested Episode:")
+print(summaries[user_ep_index])
+
+# prints recommendations
+for i in range(num_recs):
     print("\n")
+    print(summaries[closest[i]])
